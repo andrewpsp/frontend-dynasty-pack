6625a032d84fc59848c679b222010294337b1c67
Andrew S Thompson <andrew@Andrews-MacBook-Pro.local>
2018-09-09 16:01:45 -0500

    FDP back sprin initializer

---
+++ actuator-base/src/main/java/com/base/actuatorbase/ActuatorBaseApplication.java
@@ -0,0 +1,12 @@
+package com.base.actuatorbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActuatorBaseApplication.class, args);
+	}
+}
---
+++ actuator-base/src/test/java/com/base/actuatorbase/ActuatorBaseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.base.actuatorbase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ActuatorBaseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
